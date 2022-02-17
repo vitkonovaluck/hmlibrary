630f8b25edc999897a5a2dd8f881201df932ee44
vitkonovaluck <vkonovaluck@gmail.com>
2022-02-17 12:50:55 +0200

    News table migration

---
+++ database/migrations/2022_02_17_092934_news.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('news_title');
+            $table->string('news_avtor');
+            $table->dateTime('news_date');
+            $table->text('news_text');
+            $table->string('news_frame');
+            $table->string('news_frame1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
